08eab4566f83aa531afe23710319237fac0feeae
Bassel Amgad <bamgad7@gmail.com>
2022-08-27 11:50:46 +0200

    Unique salt for every user.

--- Server/Models/User.cs
+++ Server/Models/User.cs
@@ -5,16 +5,18 @@ namespace Server.Models
     public class User
     {
         public Guid Id { get; set; }
-        public string UserName { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-        public string RefreshToken { get; set; } = string.Empty;
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string RefreshToken { get; set; }
         public string RefreshTokenExpiryTime { get; set; }
 
-        public User(string userName, string password, string refreshToken, string refreshTokenExpiryTime)
+        public User(string userName, string password, byte[] passwordSalt,string refreshToken, string refreshTokenExpiryTime)
         {
             Id = Guid.NewGuid();
             UserName = userName;
             Password = password;
+            PasswordSalt = passwordSalt;
             RefreshToken = refreshToken;
             RefreshTokenExpiryTime = refreshTokenExpiryTime;
         }
