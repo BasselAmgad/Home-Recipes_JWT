b235402477b281ca695f85ea8c71d1a5a834a063
Bassel Amgad <bamgad7@gmail.com>
2022-08-27 08:55:28 +0200

    Added Guid and new constructor

--- Server/Models/User.cs
+++ Server/Models/User.cs
@@ -1,14 +1,22 @@
-﻿namespace Server.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.Models
 {
     public class User
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public Guid Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public string RefreshTokenExpiryTime { get; set; }
 
-        public User(string userName, string password)
+        public User(string userName, string password, string refreshToken, string refreshTokenExpiryTime)
         {
+            Id = Guid.NewGuid();
             UserName = userName;
             Password = password;
+            RefreshToken = refreshToken;
+            RefreshTokenExpiryTime = refreshTokenExpiryTime;
         }
     }
 }
